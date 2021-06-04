bebe512c81ad86c0a76ade41865bcfb4e8f10d5d
Csipy28 <54360998+csipy@users.noreply.github.com>
2021-06-04 12:12:18 +0200

    ready1

--- script.js
+++ script.js
@@ -6,26 +6,47 @@ const newQuoteBtn = document.getElementById('new-quote');
 
 
 
-let apiQuotes= [];
+let apiQuotes = [];
 
 //Show New Quote
 function newQuote() {
     //Pick a random quote from apiQuotes array
-const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-authorText.textContent = quote.author;
-quoteText.textContent = quote.text;
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    if (!quote.author) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quote.author;
+    }
+    if (quote.text.length >50) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+        quoteText.textContent = quote.text;
 }
 //Get Quotes from API
 async function getQuotes() {
-    const apiUrl= 'https://type.fit/api/quotes';
-    try{
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
         const response = await fetch(apiUrl);
-        apiQuotes =await response.json();
+        apiQuotes = await response.json();
         newQuote();
 
     } catch (error) {
         console.log('There is a problem')
     }
 }
+
+//TweetQuote
+function twitterQuote(){
+    const twitterURL = `https://twitter.com/intent/tweet?text=${quoteText.textContent} -  ${authorText.textContent}`;
+    window.open(twitterURL, '_blank');
+}
+
+//Event Listeners
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', twitterQuote);
+
 // On Load
 getQuotes();
